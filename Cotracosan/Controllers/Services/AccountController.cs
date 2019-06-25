392f7f19944f8ae027590b7f014ed284226314a8
altair <pxnfilo@gmail.com>
2019-06-25 13:41:31 -0600

    .

---
+++ Cotracosan/Controllers/Services/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using Cotracosan.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cotracosan.Controllers.Services
+{
+    public class AccountController : Controller
+    {
+        #region Configuración
+        private ApplicationDbContext db;
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController()
+        {
+            db = new ApplicationDbContext();
+        }
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            db = new ApplicationDbContext();
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        #endregion Fin de la configuración
+
+
+        public JsonResult IniciarSesion(string Usuario, string Contrasenia)
+        {
+            var result = SignInManager.PasswordSignIn(Usuario, Contrasenia, true, false);
+            bool sesion = result == SignInStatus.Success;
+            string socioId = "";
+            if (sesion)
+            {
+                socioId = db.Users.First(x => x.UserName == Usuario).SocioId;
+            }
+            return Json( new { login = sesion, SocioId = socioId }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Cotracosan/WebServices/GeneralServices.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GeneralServices.asmx.cs" Class="Cotracosan.WebServices.GeneralServices" %>
