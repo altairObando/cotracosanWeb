77ae527ef666e948179571de45c281711ed57825
altair <pxnfilo@gmail.com>
2019-09-04 23:19:58 -0600

    -

--- Cotracosan/Controllers/Reportes/ReportsController.cs
+++ Cotracosan/Controllers/Reportes/ReportsController.cs
@@ -20,21 +20,7 @@ namespace Cotracosan.Controllers.Reportes
         }
         // Instancia del dataset
         cotracosanWebDataSet ds = new cotracosanWebDataSet();
-        public ActionResult GetReport(string Catalogo)
-        {
-            Catalogo = "Articulos";
-            ReportViewer rv = new ReportViewer();
-            rv.ProcessingMode = ProcessingMode.Local;
-            rv.SizeToReportContent = true;
-            var adapter = new ArticulosTableAdapter();
-            adapter.Fill(ds.Articulos);
-            rv.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reportes/"+ Catalogo +".rdlc";
-            rv.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", ds.Tables[Catalogo]));
-            rv.Width = Unit.Percentage(100);
-            rv.Height = Unit.Percentage(100);
-            ViewBag.reporte = rv;
-            return View();
-        }
+        
         [HttpGet]
         public ActionResult GastosDeArticulosPorVehiculos()
         {
