6ed4ed7600279888a7fa0a1e9e860b997090cfb5
Mike Antonio Ortiz Blandon <martiz-27@hotmail.com>
2019-09-04 21:11:37 -0600

    Añadiendo el segundo reporte

--- Cotracosan/Controllers/Reportes/ReportsController.cs
+++ Cotracosan/Controllers/Reportes/ReportsController.cs
@@ -61,8 +61,30 @@ namespace Cotracosan.Controllers.Reportes
 
         }
 
+        [HttpGet]
         public ActionResult GastosDeVehiculosPorArticulos()
         {
+            ViewBag.Bus = new SelectList(db.Vehiculos, "Placa", "Placa");
+            ViewBag.reporte = null;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GastosDeVehiculosPorArticulos(string Bus, DateTime fechaInicio, DateTime fechaFin)
+        {
+            
+            ReportViewer rv = new ReportViewer();
+            rv.ProcessingMode = ProcessingMode.Local;
+            rv.SizeToReportContent = true;
+            var adapter = new Reportes2TableAdapter();
+            adapter.Fill(ds.Reportes2, Bus, fechaInicio, fechaFin);
+            rv.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reportes/Report2.rdlc";
+            rv.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", ds.Tables["Reportes2"]));
+            rv.LocalReport.SetParameters(new ReportParameter[] { new ReportParameter("placa", Bus) });
+            rv.Width = Unit.Percentage(100);
+            rv.Height = Unit.Percentage(100);
+            ViewBag.reporte = rv;
+            ViewBag.Bus = new SelectList(db.Vehiculos, "Placa", "Placa");
             return View();
         }
     }
